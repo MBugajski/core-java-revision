f40d18df9b5816da0ab10582196e560474a71bcc
MBugajski <michalbugajski@outlook.com>
2020-11-21 23:49:34 +0100

    JDBC part 2

--- 17. JDBC/src/com/MBugajski/jdbc/JDBCApplication.java
+++ 17. JDBC/src/com/MBugajski/jdbc/JDBCApplication.java
@@ -21,9 +21,13 @@ public class JDBCApplication {
 			ResultSet resultSet = statement.executeQuery("select * from employees_tbl");
 			
 			//Process the result
+			int salaryTotal = 0;
 			while(resultSet.next()) {
-				System.out.println(resultSet.getString("name"));
+				System.out.println(resultSet.getString("name") + " has salary of " +resultSet.getString("salary"));
+				salaryTotal = salaryTotal + Integer.parseInt(resultSet.getString("salary"));
 			}
+			System.out.println("Total salary payments equal " + salaryTotal);
+			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
